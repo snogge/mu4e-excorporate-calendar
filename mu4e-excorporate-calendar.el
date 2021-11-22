3a4c5b82eac69d0da03fd3787e4bd17712d626b5
Ola x Nilsson <olani@axis.com>
2021-11-23 00:45:20 +0100

    Pass data to mu4e-excorporate-calendar-accept-meeting callback
    Replace the use of exco-calendar-item-with-details-iterate with new
    function mu4e-excorporate-calendar--item-iterate.
    
    mu4e-excorporate-calendar--item-iterate is a slightly modified copy of
    exco-calendar-item-with-details-iterate that passes the
    item-identifier (which is not available through
    exco-calendar-item-with-details-iterate) and icalendar-text of each
    item to the callback.

--- mu4e-excorporate-calendar.el
+++ mu4e-excorporate-calendar.el
@@ -4,7 +4,7 @@
 
 ;; Author: Ola Nilsson <ola.nilsson@gmail.com>
 ;; Keywords: mail, calendar, excorporate, outlook, ical, mu4e
-;; Package-Requires: ((mu4e "1.6") (excorporate "1.0"))
+;; Package-Requires: ((mu4e "1.6") (excorporate "1.0") (cl-lib))
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
@@ -27,6 +27,7 @@
 
 (require 'gnus-icalendar)
 (require 'excorporate)
+(require 'cl-lib)
 
 (defun mu4e-excorporate-calendar-accept-meeting ()
   "Accept the meeting in the *Article* buffer."
@@ -38,21 +39,55 @@
        ;; defclass macro that sets variables of the same name as the
        ;; class as obsolete for reasons it seems no-one remembers.
        gnus-icalendar-event)
-     (lambda () (message "++++++ meeting done")))))
+     (lambda (_identifier item-identifier)
+       (message "Do something with %s" (pp-to-string item-identifier))))))
+
+(defun mu4e-excorporate-calendar--item-iterate (identifier response callback finalize)
+  "Iterate through calendar items in RESPONSE, calling CALLBACK on each.
+IDENTIFIER identifes the connection.
+
+CALLBACK takes the following arguments:
+FINALIZE, which is the FINALIZE argument to this function wrapped
+in a countdown.
+ITEM-IDENTIFIER, a structure representing the calendar item.  It
+should be treated as opaque.
+ICALENDAR-TEXT, the iCalendar text representing the meeting
+series."
+  ;; This is a slightly modified copy of exco-calendar-item-with-details-iterate
+  (let* ((items (exco-extract-value '(ResponseMessages
+				                      FindItemResponseMessage
+				                      RootFolder
+				                      Items)
+                                    response))
+         (countdown (length items))
+         (finalizer (lambda (&rest arguments)
+                      (when (equal (cl-decf countdown) 0)
+                        (apply finalize arguments)))))
+    (if (equal countdown 0)
+        (funcall finalize)
+      ;; TODO: exco--calendar-item-dolist is internal to excorporate
+      (exco--calendar-item-dolist
+       calendar-item items
+       (exco-calendar-item-get-details
+        identifier item-identifier
+        (lambda (icalendar-text)
+          (funcall callback finalizer item-identifier icalendar-text)))))))
 
 (defun mu4e-excorporate-calendar--make-ical-event (icalendar-text)
-  "Create a `gnus-icalendar-event' class object from ICALENDAR-TEXT."
+  "Create a class `gnus-icalendar-event' object from ICALENDAR-TEXT."
   (with-temp-buffer
     (insert icalendar-text)
     (gnus-icalendar-event-from-buffer (current-buffer))))
 
 (defun mu4e-excorporate-calendar-meeting (ical-event meeting-callback)
   "Find the exchange server meeting matching ICAL-EVENT.
-Use the icalendar UID as the key.
-Call MEETING-CALLBACK when completed."
+Use the icalendar UID as the key.  Call MEETING-CALLBACK with the
+connection identifier and item-identifier of the calendar item
+when completed."
   (message "--coan-mu4e-excp-meeting--")
   ;; Keep a local copy of the ical-event for the closures/lambdas.
-  (let ((ical (clone ical-event)))
+  (let ((ical (clone ical-event))
+        meeting-item-identifier)
     (cl-destructuring-bind
         (_sec _min _hour day month year &rest) (decode-time (gnus-icalendar-event:start-time ical))
       ;; do something like exco-org-show-day to find the outlook appointment
@@ -68,28 +103,27 @@ Call MEETING-CALLBACK when completed."
                                     callback))
        ;; exco-connection-iterate per-connection callback
        (lambda (identifier response connection-finalizer)
-         (exco-calendar-item-with-details-iterate
-          identifier response
-          ;; exco-calendar-item-with-details-iterate callback
-          (lambda (finalize
-    			   _subject _start _end _location
-    			   _main-invitees _optional-invitees
-    			   icalendar-text)
-            ;; TODO: Check some things before requesting the ical text
-            ;; from the server Needs to be careful that the item on
-            ;; the server has not been updated.  Fail in this case?
-            (let ((item-ical (mu4e-excorporate-calendar--make-ical-event icalendar-text)))
-              (when (string= (gnus-icalendar-event:uid ical)
-                             (gnus-icalendar-event:uid item-ical))
-                (message "Found %s" (gnus-icalendar-event:summary item-ical))
-                ;; TODO: Pass the meeting data to the meeting-callback
-                ))
-            (funcall finalize))
-          ;; exco-calendar-item-with-details-iterate finalize
-          connection-finalizer))
+          (mu4e-excorporate-calendar--item-iterate
+           identifier response
+           (lambda (finalize item-identifier icalendar-text)
+             ;; TODO: Check some things before requesting the ical
+             ;; text from the server Needs to be careful that the item
+             ;; on the server has not been updated.  Fail in this
+             ;; case?
+             ;; mu4e-excorporate-calendar--item-iterate will have to
+             ;; be modified to pass more details.
+             (let ((item-ical (mu4e-excorporate-calendar--make-ical-event icalendar-text)))
+               (when (string= (gnus-icalendar-event:uid ical)
+                              (gnus-icalendar-event:uid item-ical))
+                 (message "Found %s" (gnus-icalendar-event:summary item-ical))
+                 ;; TODO: Pass the meeting data to the meeting-callback
+                 (setq meeting-item-identifier item-identifier)
+                 ))
+             (funcall finalize))
+           connection-finalizer))
        ;; exco-connection-iterate finalize function
        (lambda ()
-         (funcall meeting-callback))
+         (funcall meeting-callback identifier meeting-item-identifier))
        ;; callback-will-call-finalize must be non-nil as the
        ;; per-connection callback does asynchronous calls, see the doc
        ;; of exco-connection-iterate
